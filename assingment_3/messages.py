c0f77cb1e3ee4d33e1290cba2813c9bf008124ba
lucas van Wijk <47404162+LucasVanWijk@users.noreply.github.com>
2020-07-26 17:11:07 +0200

    dynamic init

--- assingment_3/messages.py
+++ assingment_3/messages.py
@@ -1,11 +1,21 @@
 class Event:
-    def __init__(self, src, dst, name, pro_number, value):
+    def __init__(self, src, dst, name):
         self.src = src
-        self.dst = dst
         self.name = name
-        self.pro_number = pro_number
-        self.value = value
 
+class Message(Event):
+    def __init__(self, *event_var_dic, **kwargs):
+        # This allows a dict to be passed.
+        # all the keys will be turned into variables with it old value as it's value.
+        # at the same time keyword arguments are still also possible.
+        # the reason for all of this is to reduce line space at the initialization
+        # and this way all the values from a old event can be put in a dict and
+        # that dict can be passed in one go to this new event 
+        for dic in event_var_dic:
+            for key in dic:
+                setattr(self, key, dic[key])
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
 
 # class Prepare(Event):
 #     def __init__(self, src, dst, name, pro_number, value):
